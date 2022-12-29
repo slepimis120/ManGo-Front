f45b9e3f27ca14b948e530c78f9cf01a3f01dfba
saki2 <93079426+saki2@users.noreply.github.com>
2022-12-29 11:19:37 +0100

    Small fixups
    Added return button, small css changes and made the final step of the stepper be the login form popup

--- src/app/components/unregistered_components/home/home.component.ts
+++ src/app/components/unregistered_components/home/home.component.ts
@@ -3,6 +3,8 @@ import { MarkerService } from 'src/app/services/marker.service';
 import * as L from 'leaflet';
 import { MapComponent } from '../../map_components/map/map.component';
 import { VehicleType } from 'src/app/models/vehicle.model';
+import { thisMonth } from '@igniteui/material-icons-extended';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-home',
@@ -13,6 +15,9 @@ export class HomeComponent{
   price! : number;
   distance! : number;
   duration! : number;
+  modalName: bootstrap.Modal | undefined
+  loginModal: any;
+  signupModal: any;
 
 
   constructor(private markerService : MarkerService) {
@@ -32,11 +37,29 @@ export class HomeComponent{
         this.duration = Math.round(res[2]);
       }
     })}
+
+    save(){
+      this.loginModal?.toggle()
+  }
+    openloginModal(element: string | Element){
+      this.loginModal = new bootstrap.Modal(element,{} ) 
+      this.loginModal?.show()
+    }
+    opensignupModal(element: string | Element){
+      this.signupModal = new bootstrap.Modal(element,{} ) 
+      this.signupModal?.show()
+    }
   
 
   calculate_price(selected :any){
-    let selectedType = selected as VehicleType;
-    console.log(selectedType);
+    this.price = this.distance * 120;
+    if(selected == "standard"){
+      this.price += 120;
+    }else if(selected == "luxury"){
+      this.price += 200;
+    }else {
+      this.price += 180
+    }
   }
 
   callLogin(){
