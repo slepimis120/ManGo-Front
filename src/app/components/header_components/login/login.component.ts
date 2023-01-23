f29a169fff4f22609f71fa56eff650dd02038f97
Nemanja <slepimis120@gmail.com>
2023-01-23 14:41:57 +0100

    Started implementing Login
    Started connecting Login form with backend

--- src/app/components/header_components/login/login.component.ts
+++ src/app/components/header_components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { accesstoken } from '../../map_components/model/accesstoken';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +9,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor() { }
+
+  accessToken:accesstoken;
+  email:String;
+  password:String;
+  constructor(private http:HttpClient) {
+    this.accessToken = new accesstoken();
+    this.email = "";
+    this.password = "";
+   }
 
   ngOnInit(): void {
   }
+
+
+
+  public getAccessToken(){
+    let url = "http://localhost:8080/api/user/login";
+    observableAccess:new Observable<accesstoken>();
+    observableAccess = this.http.post<accesstoken>(url, {'email': this.email, 'password': this.password});
+    this.http.post<accesstoken>(url, {'email': this.email, 'password': this.password}).subscribe(data => {
+        this.accessToken.accesstoken = data.accesstoken;
+        this.accessToken.refreshtoken = data.refreshtoken;
+        console.log(this.accessToken.accesstoken);
+    }
+    );
+  }
+
+  public getUser(): Observable<accesstoken> {
+    let url = "http://localhost:8080/api/user/login";
+    return this.http.get<accesstoken>(url, {'email': this.email, 'password': this.password});
 }
+}
+
+
